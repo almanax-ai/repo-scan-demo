f2f50bead912227cdb90b4d901f3db40bb246d65
john-lawniczak <lawniczak.john@gmail.com>
2025-05-06 15:58:34 -0400

    Fixed the issue

--- src/solidity/smart_contract1.sol
+++ src/solidity/smart_contract1.sol
@@ -4,8 +4,10 @@ contract Overflow {
     uint private sellerBalance=0;
     
     function add(uint value) returns (bool){
+        require(value + sellerBalance >= sellerBalance);
         sellerBalance += value; // possible overflow
-
+        return true;
+        // Fixed!
         // possible auditor assert
         // assert(sellerBalance >= value); 
     } 
