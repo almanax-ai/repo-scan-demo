ff7b286a95a0f44798deff51f6279e499adcf68d
john-lawniczak <lawniczak.john@gmail.com>
2025-05-06 14:26:29 -0400

    Fix withdraw vulnerability

--- src/solidity/smart_contract2.sol
+++ src/solidity/smart_contract2.sol
@@ -20,7 +20,7 @@ contract Wallet {
     }
     
     function withdraw(uint256 amount) public {
-        require(amount >= balances[msg.sender]);
+        require(amount <= balances[msg.sender]);
         msg.sender.transfer(amount);
         balances[msg.sender] -= amount;
     }
